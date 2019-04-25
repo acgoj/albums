8ad2dacf68e2f242afbb54e1a99462606b23669a
Antonio Oliveira <acgoj@cin.ufpe.br>
2019-04-25 07:59:53 -0300

    Ajustes config

--- android/app/src/main/java/com/albums/MainApplication.java
+++ android/app/src/main/java/com/albums/MainApplication.java
@@ -3,10 +3,14 @@ package com.albums;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,10 +26,19 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNGestureHandlerPackage()
       );
     }
-
+@Override
++  protected ReactActivityDelegate createReactActivityDelegate() {
++    return new ReactActivityDelegate(this, getMainComponentName()) {
++      @Override
++      protected ReactRootView createRootView() {
++       return new RNGestureHandlerEnabledRootView(MainActivity.this);
++      }
++    };
++  }
     @Override
     protected String getJSMainModuleName() {
       return "index";
