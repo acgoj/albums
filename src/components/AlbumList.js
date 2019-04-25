bddc527464f26b09781881a2ac7d97cb49310599
Antonio Oliveira <acgoj@cin.ufpe.br>
2019-04-24 22:35:54 -0300

    Ajuste Para apresentação

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
-  
   state = { albums: [] };
 
   componentWillMount() {
@@ -12,15 +12,18 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text>{album.title}</Text>);
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
     console.log(this.state);
+
     return (
-      <View>
-        { this.renderAlbums() }
-      </View>
+      <ScrollView horizontal contentContainerStyle={{marginTop: 80}}>
+        {this.renderAlbums()}
+      </ScrollView>
     );
   }
 }
